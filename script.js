aef3712bd50b69ca5c540fda63c0c281bf79ff1d
ramon1997 <kratosramonsantana@gmail.com>
2023-06-08 14:28:51 -0300

    mudanças no estilo

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ async function busca() {
 
 busca().then(data => {
     const dados = data.USDBRL.ask;
-    const name = data.USDBRL.name;
+   // const name = data.USDBRL.name;
     const por = data.USDBRL.pctChange;
     const max = data.USDBRL.high;
     const min = data.USDBRL.low;
@@ -16,7 +16,7 @@ busca().then(data => {
     const dadoredondo= parseFloat(dados).toFixed(2);
 
     document.getElementById("valor").innerHTML = dadoredondo;
-    document.getElementById("name").innerHTML = name;
+    //document.getElementById("name").innerHTML = name;
     document.getElementById("por").innerHTML = por;
     document.getElementById("max").innerHTML = maxredondo;
     document.getElementById("min").innerHTML = minredondo;
